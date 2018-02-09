b1a97d3eaddbb62d926b3733023cd9b1aac6953e
cpadilla <christoferpadilla@gmail.com>
2018-02-08 20:55:59 -0500

    Created application called posts

---
+++ posts/client/src/components/Main.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <a href="/" target="_blank" >
+          Art
+        </a>
+      </li>
+      <li>
+        <a href="/" target="_blank">
+          Games
+        </a>
+      </li>
+      <li>
+        <a href="/Blog" target="_blank">
+          Blog
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Hi! My name is Chris.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
