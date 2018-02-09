8b09a15026a6853efb9c830aab527b33cd6dc25c
cpadilla <christoferpadilla@gmail.com>
2018-02-09 16:07:22 -0500

    Adding placeholder text for our tweets vue

--- posts/client/src/App.vue
+++ posts/client/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="container">
+      <span id="tweet">Lorem ipsum dolor sit amet, nonummy ligula volutpat hac integer nonummy. Suspendisse ultricies, congue etiam tellus, erat libero, nulla eleifend, mauris pellentesque. Suspendisse integer praesent vel, integer gravida mauris, fringilla vehicula lacinia non</span>
+    </div>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -19,5 +22,24 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#container {
+  position: relative;
+  height: 100px;
+  width: 400px;
+  line-height: 100px;
+  text-align: center;
+  left: 50%;
+  margin-left: -200px;
+}
+#tweet {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  display: inline-block;
+  vertical-align: middle;
+  line-height: normal;
 }
 </style>
