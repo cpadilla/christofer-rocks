0f96d23e7b43c0eb37b6a58db8d055e370194fb4
Christofer Padilla <chris@nzxt.lan>
2020-12-31 07:32:23 -0500

    Added a new blog post called My Love of Comics.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,6 +3,9 @@ module.exports = {
     description: 'A glimpse into my personal reflections.',
     themeConfig: {
         logo: '/images/logo.png',
+        nav: [
+            { text: 'Articles', link: '/blog/' }
+        ],
         nextLinks: true,
         prevLinks: true
     }
